71aa3271629ae722cde38b1551a4223f79e3ed4b
Joel LaRussa <joellarussa@gmail.com>
2024-08-15 15:13:23 -0700

    Added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import ProjectsSection from './components/ProjectsSection';
+import ContactForm from './components/ContactForm';
+import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <HeroSection />
+            <ProjectsSection />
+            <ContactForm />
+            <footer style={{ backgroundColor: '#000', color: 'white', textAlign: 'center', padding: '1rem' }}>
+                <p>Joel™ 2024</p>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
